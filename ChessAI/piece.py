ce4a0eab848c2c515776083c462f3551ae4c032e
NgDucLoc <ngducloc1112002@gmail.com>
2022-11-27 21:11:07 +0700

    .

---
+++ ChessAI/piece.py
@@ -0,0 +1,20 @@
+import pygame as p
+from const import *
+
+
+IMAGES = {}
+def loadImages():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK',
+              'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load(
+            "images/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
+
+def drawPieces(screen, board):
+    for row in range(DIMENSION):
+        for column in range(DIMENSION):
+            piece = board[row][column]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(
+                    column * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
